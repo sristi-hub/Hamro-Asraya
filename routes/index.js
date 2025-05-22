c685350c10a8d3b16450e6a408d3afb29b3755db
Asmitapaudel <asmeetapdl@gmail.com>
2025-05-22 20:28:14 +0545

    UI for hostel list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const hostelData=require("../models/hostel")
 
-/* GET home page. */
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+// Route to display list of Hotels
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+// Route to render hostels using static data
+router.get('/hostels', function (req, res, next) {
+  res.render('Home/hostel_list', {
+    title: 'Hostel List',
+    hostels: hostelData.hostels
+  });
+});
 module.exports = router;
